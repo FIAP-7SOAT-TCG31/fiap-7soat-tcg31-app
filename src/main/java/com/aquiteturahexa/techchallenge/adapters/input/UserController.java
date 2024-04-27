8d77fe94271758b39d3708f2df5e78b5c45346f8
Julio <julio.mech@hotmail.com>
2024-04-27 09:40:41 -0300

    remove beans

--- src/main/java/com/aquiteturahexa/techchallenge/adapters/input/UserController.java
+++ src/main/java/com/aquiteturahexa/techchallenge/adapters/input/UserController.java
@@ -3,7 +3,12 @@ package com.aquiteturahexa.techchallenge.adapters.input;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +19,6 @@ import com.aquiteturahexa.techchallenge.core.model.User;
 @RequestMapping("/")
 public class UserController {
 
-    @Autowired
     private UserServices userservices;
 
     @GetMapping("/ola")
@@ -22,8 +26,28 @@ public class UserController {
         return "Ola imbecil";
     }
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     public List<User> getAllUsers() {
         return userservices.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userservices.getUserById(id);
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        return userservices.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userservices.updateUser(user, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userservices.deleteUser(id);
     }
 }
--- src/main/java/com/aquiteturahexa/techchallenge/application/UserServices.java
+++ src/main/java/com/aquiteturahexa/techchallenge/application/UserServices.java
@@ -2,22 +2,41 @@ package com.aquiteturahexa.techchallenge.application;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.aquiteturahexa.techchallenge.adapters.output.UserRepository;
 import com.aquiteturahexa.techchallenge.core.model.User;
 
-@Service
 public class UserServices {
 
-    @Autowired
     private UserRepository userRepository;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-    /*
-     * ....
-     */
+
+    public User getUserById(Long id) {
+
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user, Long id) {
+        User existeUser = userRepository.findById(id);
+
+        if (existeUser != null) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+
+        // Tratar retorno null
+        return null;
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+
+    }
 }
