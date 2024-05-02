6c08dff6392ede9b84c78684b0cdb153fc9618d0
Julio <julio.mech@hotmail.com>
2024-05-02 15:11:02 -0300

    change annotations

--- src/main/java/com/aquiteturahexa/techchallenge/adapters/controllers/UserController.java
+++ src/main/java/com/aquiteturahexa/techchallenge/adapters/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.aquiteturahexa.techchallenge.adapters.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aquiteturahexa.techchallenge.core.model.User;
@@ -38,7 +36,7 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User findById(@RequestParam Long id) {
+    public User findById(@PathVariable Long id) {
 
         return userServicePort.findById(id);
     }
