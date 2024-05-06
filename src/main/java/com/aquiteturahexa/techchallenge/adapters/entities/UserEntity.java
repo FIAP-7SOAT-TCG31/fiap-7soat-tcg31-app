296c158fe49cb0773480da7bbdd8076fbd08c467
Julio <julio.mech@hotmail.com>
2024-05-06 11:15:54 -0300

    add atribute to user

--- src/main/java/com/aquiteturahexa/techchallenge/adapters/entities/UserEntity.java
+++ src/main/java/com/aquiteturahexa/techchallenge/adapters/entities/UserEntity.java
@@ -20,6 +20,7 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long CPF;
     private String name;
     private String email;
 
