c540a3575fdf11a865ae27110530be5208b1ed58
gm50x <getuliomagela@outlook.com>
2024-05-18 15:27:51 -0300

    feat: adds list item by type input port

--- src/main/java/com/aquiteturahexa/techchallenge/core/ports/in/ListItemsPortIn.java
+++ src/main/java/com/aquiteturahexa/techchallenge/core/ports/in/ListItemsPortIn.java
@@ -3,7 +3,10 @@ package com.aquiteturahexa.techchallenge.core.ports.in;
 import java.util.List;
 
 import com.aquiteturahexa.techchallenge.core.model.Item;
+import com.aquiteturahexa.techchallenge.core.model.ItemType;
 
 public interface ListItemsPortIn {
     List<Item> getAll();
+
+    List<Item> getAllByType(ItemType itemType);
 }
