230bb2f0b75ef8e99f54c1f1d4cfeca4adf21f12
gm50x <getuliomagela@outlook.com>
2024-05-18 15:28:32 -0300

    feat: adds list item by type output port

--- src/main/java/com/aquiteturahexa/techchallenge/adapters/persistence/ListItemsPersistanceAdapterOut.java
+++ src/main/java/com/aquiteturahexa/techchallenge/adapters/persistence/ListItemsPersistanceAdapterOut.java
@@ -2,11 +2,14 @@ package com.aquiteturahexa.techchallenge.adapters.persistence;
 
 import java.util.List;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 import com.aquiteturahexa.techchallenge.adapters.persistence.mapper.ItemMapper;
 import com.aquiteturahexa.techchallenge.adapters.persistence.repositories.ItemRepository;
+import com.aquiteturahexa.techchallenge.adapters.persistence.specifications.ItemSpecification;
 import com.aquiteturahexa.techchallenge.core.model.Item;
+import com.aquiteturahexa.techchallenge.core.model.ItemType;
 import com.aquiteturahexa.techchallenge.core.ports.out.ListItemsPortOut;
 
 import lombok.RequiredArgsConstructor;
@@ -22,5 +25,12 @@ public class ListItemsPersistanceAdapterOut implements ListItemsPortOut {
                 var entities = itemRepository.findAll();
                 return entities.stream().map(ItemMapper::toDomain).toList();
 
+        }
+
+        @Override
+        public List<Item> getAllByType(ItemType itemType) {
+                var entities = itemRepository.findAll(Specification.where(
+                                ItemSpecification.hasItemType(itemType.toString())));
+                return entities.stream().map(ItemMapper::toDomain).toList();
         }
 }
