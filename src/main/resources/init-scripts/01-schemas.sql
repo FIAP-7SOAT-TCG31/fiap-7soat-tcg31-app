bc3befdf23b9ab0f44fc3a5258e25c363add349a
Lucas Lima <lucaslima_miml@hotmail.com>
2024-05-20 22:05:48 -0300

    chore: include SQL statements

--- src/main/resources/init-scripts/01-schemas.sql
+++ src/main/resources/init-scripts/01-schemas.sql
@@ -42,4 +42,3 @@ CREATE TABLE tb_order_items (
     FOREIGN KEY (item_id) REFERENCES tb_items(id)
 );
 
-INSERT INTO "tb_users" ("email","name","password","role","username") VALUES ('admin@admin.com','Administrator','$2a$10$WLrGTAB4pctxD/KMr26quuf/4dtZgvqX241EMy1QOXuAG9a9uu052','ADMIN','admin');
