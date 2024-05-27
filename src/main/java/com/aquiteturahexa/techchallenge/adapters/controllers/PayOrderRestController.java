71d91dabbb599980da112ebb44b2ac470ab8a5ef
Lucas Lima <lucaslima_miml@hotmail.com>
2024-05-27 14:02:04 -0300

    feat: permit swagger and add tags

--- src/main/java/com/aquiteturahexa/techchallenge/adapters/controllers/PayOrderRestController.java
+++ src/main/java/com/aquiteturahexa/techchallenge/adapters/controllers/PayOrderRestController.java
@@ -1,9 +1,13 @@
 package com.aquiteturahexa.techchallenge.adapters.controllers;
 
+import com.aquiteturahexa.techchallenge.adapters.controllers.dto.ResponseFollowupDto;
 import com.aquiteturahexa.techchallenge.adapters.controllers.mappers.OrderMapper;
 import com.aquiteturahexa.techchallenge.core.ports.in.EffecitvePaymentPortIn;
 import com.aquiteturahexa.techchallenge.core.ports.in.GetOrderPortIn;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,7 +31,13 @@ public class PayOrderRestController {
     @PatchMapping(path = "/api/v1/orders/{id}/payment")
     @Operation(summary = "Receive order payment")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully paid order"),
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully paid order",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseFollowupDto.class))
+            ),
             @ApiResponse(responseCode = "404", description = "Order not found")
     })
     public ResponseEntity<?> create(@RequestHeader Map<String, String> headers, @PathVariable("id") String id) {
