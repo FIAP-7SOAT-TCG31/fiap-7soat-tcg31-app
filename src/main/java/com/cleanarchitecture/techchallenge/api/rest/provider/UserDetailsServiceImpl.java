18af37a86caf090a0d4e00ad9c7d6fe6269dc01f
Lucas Lima <lucaslima_miml@hotmail.com>
2024-09-23 20:45:17 -0300

    feat(token): Changes in filters

--- src/main/java/com/cleanarchitecture/techchallenge/api/rest/provider/UserDetailsServiceImpl.java
+++ src/main/java/com/cleanarchitecture/techchallenge/api/rest/provider/UserDetailsServiceImpl.java
@@ -1,7 +1,9 @@
 package com.cleanarchitecture.techchallenge.api.rest.provider;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,7 +12,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -22,6 +25,21 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         String[] tokenParts = username.split("\\.");
         String payload = new String(Base64.getUrlDecoder().decode(tokenParts[1]));
         JSONObject jsonObject = new JSONObject(payload);
-        return new User(jsonObject.getString("custom:cpf"), jsonObject.getString("custom:cpf"), List.of(new SimpleGrantedAuthority(jsonObject.getString("custom:role"))));
+
+        try {
+            if (StringUtils.isBlank(jsonObject.getString("custom:cpf"))) {
+                throw new BadCredentialsException("Token not valid");
+            }
+        } catch (Exception e) {
+            return new User(
+                    "ERROR",
+                    "ERROR",
+                    new HashSet<>());
+        }
+
+        return new User(
+                jsonObject.getString("custom:cpf"),
+                jsonObject.getString("sub"),
+                Set.of(new SimpleGrantedAuthority(jsonObject.getString("custom:role").toUpperCase())));
     }
 }
--- src/main/java/com/cleanarchitecture/techchallenge/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/com/cleanarchitecture/techchallenge/configuration/ApplicationSecurityConfiguration.java
@@ -1,69 +1,38 @@
 package com.cleanarchitecture.techchallenge.configuration;
 
 import com.cleanarchitecture.techchallenge.api.rest.provider.JwtAuthFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-
+@RequiredArgsConstructor
 public class ApplicationSecurityConfiguration {
 
-    private static final String[] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {};
-    private static final String[] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {};
-    private static final String[] ENDPOINTS_ADMIN = {};
-
-    @Autowired
-    JwtAuthFilter jwtAuthFilter;
-    @Autowired
-    UserDetailsService userDetailsService;
-
+    private static final String[] ENDPOINTS_ADMIN = {"api/v1/items"};
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeHttpRequests()
-                .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
-                .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
-                .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMIN")
-                .anyRequest().denyAll()
+                .authorizeHttpRequests()
+                .requestMatchers(ENDPOINTS_ADMIN).hasAuthority("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
 }
