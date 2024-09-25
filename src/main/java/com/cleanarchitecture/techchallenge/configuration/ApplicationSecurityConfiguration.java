032dbb6d78d085c170e059320daee7bf44b0d23f
gm50x <getuliomagela@outlook.com>
2024-09-25 20:30:11 -0300

    feat: improves authorization configuration

--- src/main/java/com/cleanarchitecture/techchallenge/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/com/cleanarchitecture/techchallenge/configuration/ApplicationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.cleanarchitecture.techchallenge.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +20,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApplicationSecurityConfiguration {
 
-    private static final String[] ENDPOINTS_ADMIN = { "api/v1/items" };
+    private static final String[] ENDPOINTS_ADMIN = {
+            "api/v1/clients",
+            "api/v1/clients/{id}"
+    };
     private static final String[] ENDPOINTS_OPEN = {
             "api/v1/followup",
             "api/v1/orders/{id}/payment/{status}"
@@ -30,7 +34,11 @@ public class ApplicationSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers(ENDPOINTS_ADMIN).hasAuthority("ADMIN")
+                .requestMatchers(ENDPOINTS_ADMIN).hasAnyAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "api/v1/items").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "api/v1/items/{id}").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.GET, "api/v1/items").permitAll()
+                .requestMatchers(HttpMethod.GET, "api/v1/items/{id}").permitAll()
                 .requestMatchers(ENDPOINTS_OPEN).permitAll()
                 .anyRequest()
                 .authenticated()
